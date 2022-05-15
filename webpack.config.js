ef6c26a16d28b30b68761e2140a15f69453597a4
Mbah Dhaim <mas.dhaim#gmail.com>
2022-05-15 01:08:29 +0000

    update webpack config and ignore compiled

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'app'),
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
